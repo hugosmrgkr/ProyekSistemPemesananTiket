7162e677e32ecc98b1c8bb7cf5698fb6385f1810
hugosmrgkr <hugosimorangkir10@gmail.com>
2025-12-03 09:37:54 +0700

    sesuaikan class diagram + implementasi code model

--- src/com/tiket/model/Admin.java
+++ src/com/tiket/model/Admin.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.tiket;
+package com.tiket.model;
 
 /**
  *
  * @author TUF GAMING
  */
-public class Main {
+public class Admin {
     
-
 }
